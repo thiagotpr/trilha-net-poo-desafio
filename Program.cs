1cdc5eec784c370b63e21d06204b792ee6fd5597
Thiago Pereira <thiagopr.tpr@gmail.com>
2024-01-07 23:26:44 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // TODO: Realizar os testes com as classes Nokia e Iphone
+        Nokia nokia = new Nokia("123456789", "Nokia 8", "IMEI123", 64);
+        Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI456", 128);
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
